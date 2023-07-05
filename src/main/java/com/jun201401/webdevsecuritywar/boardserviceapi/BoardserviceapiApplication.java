cee78ee09fcf3d63bee309d41bc1b0e48dd85c9f
jun201401 <jun.ti.gear@gmail.com>
2023-07-05 16:56:23 +0900

    :tada: Initial commit

---
+++ src/main/java/com/jun201401/webdevsecuritywar/boardserviceapi/BoardserviceapiApplication.java
@@ -0,0 +1,13 @@
+package com.jun201401.webdevsecuritywar.boardserviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardserviceapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardserviceapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jun201401/webdevsecuritywar/boardserviceapi/BoardserviceapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jun201401.webdevsecuritywar.boardserviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardserviceapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
